aee4115cce262245675f1d1639638b80a75d99bf
GonzaloDavid <davidgonzalomejia@hotmail.com>
2018-08-06 15:11:50 -0500

    Añadimos que envie condicion puerta, luz,alarma

--- Backend-Sails/backend/api/controllers/ControlController.js
+++ Backend-Sails/backend/api/controllers/ControlController.js
@@ -10,17 +10,29 @@ module.exports = {
    // npm install request --save
     const request = require('request');
     console.log('Enviar datos');
+    var str='';
+    if(req.body.name.localeCompare('alarma'))
+    {
+      str='alarmaI';
+    }if(req.body.name.localeCompare('luz'))
+    {
+      str='luzI';
+    }if(req.body.name.localeCompare('puerta'))
+    {
+      str='puertaI';
+    }
+    console.log('El valor de str es',str);
+      const objetoRequest = {
 
-    const objetoRequest = {
-      url: 'http://localhost:1338/usuarioRasberry',
-      form: {
-        nombre: req.body.nombre,
-        accion: req.body.accion,
+        url: 'http://192.168.43.195:1337/'+str,
+        form: {
+          nombre: req.body.nombre,
+          accion: req.body.accion,
 
-      }
-    };
+        }
+      };
     const objetoRequest1 = {
-      url: 'http://localhost:1339/control',
+      url: 'http://localhost:1338/control',
       form: {
         nombre: req.body.nombre,
         accion: req.body.accion,
